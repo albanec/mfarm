4aed3fdb7ae884b4a53f696f2e5c12a3e1e86704
Evgeni <eugeni.maksimov@yandex.ru>
2017-06-13 00:34:32 +0000

    Create new_kernel.sh

--- rig/new_kernel.sh
+++ rig/new_kernel.sh
@@ -2,6 +2,7 @@
 
 mkdir new_kernel
 cd new_kernel
+wget kernel.ubuntu.com/~kernel-ppa/mainline/v4.12-rc5/linux-headers-4.12.0-041200rc5_4.12.0-041200rc5.201706112031_all.deb
 wget kernel.ubuntu.com/~kernel-ppa/mainline/v4.12-rc5/linux-headers-4.12.0-041200rc5-generic_4.12.0-041200rc5.201706112031_amd64.deb
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.12-rc5/linux-image-4.12.0-041200rc5-generic_4.12.0-041200rc5.201706112031_amd64.deb
 dpkg -i *.deb
