41c826e71783b65e472dd959b1e6ac2dc7bda171
Evgeni <eugeni.maksimov@yandex.ru>
2017-06-15 21:41:31 +0000

    Create start.sh

--- rig/start.sh
+++ rig/start.sh
@@ -1,37 +1,15 @@
 #!/bin/bash
 MINER_PATH='/home/rig_admin/claymore_miner_v9.3'
-
+CARDS_COLOR='red'
 tmux new-session -d -n mining
-
 tmux send-keys -t mining "cd $MINER_PATH
 "
-
-tmux send-keys -t mining "./start.bash
+tmux send-keys -t mining "./start.sh
 "
-
 sleep 10
-# увеличение скорости кулеров
-for n in 0 1 2 3 4 5; do
-echo 'Setting fan speed for GPU #'$n' to 80%';
-echo 200 > /sys/class/drm/card$n/device/hwmon/hwmon$n/pwm1;
-done;
 
-# sleep 10
-# разгон
-sclk=0
-mclk=0
-for n in 0 1 2 3 4 5; do
-echo 'Overclocking settings for GPU #'$n;
-echo $sclk > /sys/class/drm/card$n/device/pp_sclk_od;
-echo $mclk > /sys/class/drm/card$n/device/pp_mclk_od;
-done;
-
-## оптимизация питания
-for n in 0 1 2 3 4 5; do
-echo 'Power settings for GPU #'$n;
-echo auto > /sys/class/drm/card$n/device/power_dpm_force_performance_level
-#echo 6 > /sys/class/drm/card$n/device/pp_dpm_sclk;
-done;
+# oc 
+sh ./oc_$CARDS_COLOR.sh
 
 sleep 2
 # screen -r
