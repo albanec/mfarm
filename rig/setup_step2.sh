4200b470d75a2b192ce856b7e89c42bdaeee7463
Evgeni <eugeni.maksimov@yandex.ru>
2017-06-15 21:30:08 +0000

    Create setup_step2.sh

--- rig/setup_step2.sh
+++ rig/setup_step2.sh
@@ -155,15 +155,4 @@ echo 'fs.inotify.max_queued_events = 65536' >> /etc/sysctl.conf
 
 sysctl -p
 
-### miner setup
-cd ~
-mkdir miners
-mkdir miners/Claymore_ETH_v9.5
-mkdir miners/Claymore_ZEC_v12.5
-mkdir miners/EWBF_ZEC_0.3.3b
-tar -xvzf Claymore_ETH_v9.5.tar.gz -C miners/Claymore_ETH_v9.5/
-tar -xvzf Claymore_ZEC_v12.5.tar.gz -C miners/Claymore_ZEC_v12.5/
-tar -xvzf EWBF_ZEC_0.3.3b.tar.gz -C miners/EWBF_ZEC_0.3.3b/
-chown rig_admin:rig_admin miners -R
-
 exit
