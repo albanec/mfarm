ec08d456fa8f3628a84f6ecd1ce09dad2672c83f
Evgeni <eugeni.maksimov@yandex.ru>
2017-06-15 21:03:26 +0000

    Create setup_step2.sh

--- rig/setup_step2.sh
+++ rig/setup_step2.sh
@@ -164,3 +164,6 @@ mkdir miners/EWBF_ZEC_0.3.3b
 tar -xvzf Claymore_ETH_v9.5.tar.gz -C miners/Claymore_ETH_v9.5/
 tar -xvzf Claymore_ZEC_v12.5.tar.gz -C miners/Claymore_ZEC_v12.5/
 tar -xvzf EWBF_ZEC_0.3.3b.tar.gz -C miners/EWBF_ZEC_0.3.3b/
+chown rig_admin:rig_admin miners -R
+
+exit
