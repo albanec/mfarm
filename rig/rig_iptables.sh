b2b04990b3ee550325e797bbd7cec7c0fe9a2e1f
Evgeni <eugeni.maksimov@yandex.ru>
2017-06-15 19:27:11 +0000

    Create rig_iptables.sh

--- rig/rig_iptables.sh
+++ rig/rig_iptables.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+INT_NAME='enp7s0'
+
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 #ssh
-iptables -A INPUT -i enp7s0 -p tcp -m tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -i enp7s0 -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -A INPUT -i $INT_NAME -p tcp -m tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i $INT_NAME -p tcp -m tcp --dport 22 -j ACCEPT
 iptables -A INPUT -i tun0 -p tcp -m tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A INPUT -i tun0 -p tcp -m tcp --dport 22 -j ACCEPT
 #dhcp
-iptables -A INPUT -i enp7s0 -p udp -m udp --dport 67 -j ACCEPT
-iptables -A INPUT -i enp7s0 -p tcp -m tcp --dport 67 -j ACCEPT
+iptables -A INPUT -i $INT_NAME -p udp -m udp --dport 67 -j ACCEPT
+iptables -A INPUT -i $INT_NAME -p tcp -m tcp --dport 67 -j ACCEPT
 
 iptables -P INPUT DROP
 iptables -P FORWARD ACCEPT
