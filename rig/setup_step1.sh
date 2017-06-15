856a36d36d47720ff5e3d577cded3ffc1dbafe24
Evgeni <eugeni.maksimov@yandex.ru>
2017-06-15 21:07:37 +0000

    Create setup_step1.sh

--- rig/setup_step1.sh
+++ rig/setup_step1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir new_kernel
-cd new_kernel
+mkdir kernel
+cd kernel
 wget kernel.ubuntu.com/~kernel-ppa/mainline/v4.12-rc5/linux-headers-4.12.0-041200rc5_4.12.0-041200rc5.201706112031_all.deb
 wget kernel.ubuntu.com/~kernel-ppa/mainline/v4.12-rc5/linux-headers-4.12.0-041200rc5-generic_4.12.0-041200rc5.201706112031_amd64.deb
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.12-rc5/linux-image-4.12.0-041200rc5-generic_4.12.0-041200rc5.201706112031_amd64.deb
