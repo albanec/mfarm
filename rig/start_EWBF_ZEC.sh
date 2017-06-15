e78fdec544722a3bcbfee783a34dba08b7820162
Evgeni <eugeni.maksimov@yandex.ru>
2017-06-15 21:51:19 +0000

    Create start_EWBF_ZEC.sh

---
+++ rig/start_EWBF_ZEC.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wal=''
+worker='rig2'
+email=''
+pool='zec-eu1.nanopool.org:9999'
+pswd='z'
+port=6666
+
+./miner --server $zpool --user $zwal.$worker/$email --pass $pswd --port $port --log --templimit 80 
