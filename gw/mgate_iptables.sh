13489d43c062db9d8079f65b879928b34bd3a984
Evgeni <eugeni.maksimov@yandex.ru>
2017-06-16 16:50:04 +0000

    Update mgate_iptables.sh

--- gw/mgate_iptables.sh
+++ gw/mgate_iptables.sh
@@ -9,12 +9,13 @@ iptables -A INPUT -i tun0 -p tcp -m tcp --dport 22 -m conntrack --ctstate NEW,ES
 iptables -A INPUT -i tun0 -p tcp -m tcp --dport 22 -j ACCEPT
 #ovpn
 iptables -A INPUT -i eth0 -p udp -m udp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i eth0 -p tcp -m tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 # dhcp
 iptables -A INPUT -i eth0 -p udp -m udp --dport 67 -j ACCEPT
 iptables -A INPUT -i eth0 -p tcp -m tcp --dport 67 -j ACCEPT
 #цепочка postrouting
 iptables -t nat -A POSTROUTING -s 10.10.0.0/24 -o eth0 -j MASQUERADE
-
+sudo iptables -t nat -A POSTROUTING -s 192.168.88.0/24 -o eth0 -j MASQUERADE
 
 iptables -P INPUT DROP
 iptables -P FORWARD ACCEPT
