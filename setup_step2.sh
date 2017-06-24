26e52561a2b6db3f2a60eebed68e7295eb99c2a8
Evgeni <eugeni.maksimov@yandex.ru>
2017-06-24 03:50:52 +0000

    Update setup_step2.sh

--- setup_step2.sh
+++ setup_step2.sh
@@ -9,12 +9,11 @@ mkdir cuda
 cd cuda
 echo 'Download CUDA'
 echo ''
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run
 echo 'Install CUDA'
 echo ''
-sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-sudo apt-get update
-sudo apt-get install cuda
+chmod +x cuda_8.0.61_375.26_linux-run
+sudo ./cuda_8.0.61_375.26_linux-run
 echo 'System reboot'
 echo ''
 sleep 5
@@ -25,4 +24,9 @@ if [ $install_driver == 'TRUE' ]
 then
 mkdir driver
 cd driver
+wget http://ru.download.nvidia.com/XFree86/Linux-x86_64/375.66/NVIDIA-Linux-x86_64-375.66.run
+chmod +x NVIDIA-Linux-x86_64-375.66.run
+sudo ./NVIDIA-Linux-x86_64-375.66.run
+fi
 
+exit
