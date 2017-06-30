5d9d19dd86f03768b195e08fa0cbd8b99075c1c2
Evgeni <eugeni.maksimov@yandex.ru>
2017-06-30 14:51:14 +0000

    Update setup_step2.sh

--- setup_step2.sh
+++ setup_step2.sh
@@ -3,10 +3,16 @@
 install_cuda='TRUE'
 install_driver='FALSE'
 
-apt-get install build-essentialxserver-xorg  libgtk-3-dev xdm
-sudo apt-get install -y build-essential xserver-xorg xorg-dev dkms xdm libgtk-3-0
+apt-get install -y build-essential xserver-xorg build-essential xserver-xorg dkms libgtk-3-0
 
-/etc/init.d/xdm stop
+if [ $install_driver == 'TRUE' ]
+then
+mkdir driver
+cd driver
+wget http://ru.download.nvidia.com/XFree86/Linux-x86_64/375.66/NVIDIA-Linux-x86_64-375.66.run
+chmod +x NVIDIA-Linux-x86_64-375.66.run
+sudo ./NVIDIA-Linux-x86_64-375.66.run
+fi
 
 if [ $install_cuda == 'TRUE' ]
 then
@@ -18,20 +24,11 @@ wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8
 echo 'Install CUDA'
 echo ''
 chmod +x cuda_8.0.61_375.26_linux-run
-sudo ./cuda_8.0.61_375.26_linux-run
+sudo ./cuda_8.0.61_375.26_linux-run --silent --toolkit --samples --samplespath=/usr/local/cuda-8.0/samples --override
 echo 'System reboot'
 echo ''
 sleep 5
 sudo reboot now
-fi
-
-if [ $install_driver == 'TRUE' ]
-then
-mkdir driver
-cd driver
-wget http://ru.download.nvidia.com/XFree86/Linux-x86_64/375.66/NVIDIA-Linux-x86_64-375.66.run
-chmod +x NVIDIA-Linux-x86_64-375.66.run
-sudo ./NVIDIA-Linux-x86_64-375.66.run
 fi
 
 sudo nvidia-xconfig -s -a --force-generate --allow-empty-initial-configuration --cool-bits=32 \
