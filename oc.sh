839bad4730de86e7689d58a52056714de19caed6
Evgeni <eugeni.maksimov@yandex.ru>
2017-07-10 17:00:27 +0000

    Update oc.sh

--- oc.sh
+++ oc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export DISPLAY=:0
-CLOCK=160 # 160
+CLOCK=120 # 160
 MEM=1000 # 2000
 
 #echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
@@ -9,14 +9,14 @@ MEM=1000 # 2000
 #echo 2800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 #echo 2800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 
-for i in {0..5}
+for i in {0..7}
     do
         sudo nvidia-smi -i $i -pm 0
-        sudo nvidia-smi -i $i -pl 170
+        sudo nvidia-smi -i $i -pl 100
         # nvidia-smi -i $i -ac 4004,1911
         nvidia-settings -c :0 -a [gpu:$i]/GPUPowerMizerMode=1
-        nvidia-settings -c :0 -a [gpu:$i]/GPUFanControlState=1
-        nvidia-settings -c :0 -a [fan:$i]/GPUTargetFanSpeed=70
+        nvidia-settings -c :0 -a [gpu:$i]/GPUFanControlState=0
+        #nvidia-settings -c :0 -a [fan:$i]/GPUTargetFanSpeed=80
         nvidia-settings -c :0 -a [gpu:$i]/GPUGraphicsClockOffset[3]=$CLOCK
         nvidia-settings -c :0 -a [gpu:$i]/GPUMemoryTransferRateOffset[3]=$MEM
     done
